408dcd123ece9a92ec6a39bfc7a066ff72f06098
Matheus <matheusolivsilv.job@gmail.com>
2023-04-11 18:21:29 -0300

    mudanca no relatorio e fiz outro teste.

--- lab1/lab1.c
+++ lab1/lab1.c
@@ -2,15 +2,13 @@
 #include <pthread.h>
 
 #define numero_de_threads 2 
-#define array_size 101
+#define array_size 10000
 
 int array[array_size];
 
 void init_array() {
-    printf("Array inicial: ");
     for (int i = 0; i < array_size; i++) {
         array[i] = i;
-        printf("%d ", array[i]);
     }   
     printf("\n");
 }
@@ -29,9 +27,27 @@ void *print_message(void *thread_id) {
     pthread_exit(NULL);
 }
 
+// faz uma função para conferir se o array global foi corretamente multiplicado por 2
+void testa_lab1(int *copia_array){
+    for (int i = 0; i < array_size; i++) {
+        if(array[i] != 2 * copia_array[i]){
+            printf("Erro no array na posição %d", i);
+            return;
+        }
+    }
+    printf("Array multiplicado por 2 corretamente");
+}
+
 int main()
 {
     init_array(); // começa o array com valores de 0 a array_size-1
+    int copia_array[array_size]; // cria um array para copiar o array global e compararmos depois.
+    
+    // Copia os elementos do array
+    for (int i = 0; i < array_size; i++) {
+        copia_array[i] = array[i];
+    } 
+
     pthread_t threads[numero_de_threads]; 
 
     for (long i = 0; i < numero_de_threads; i++) {
@@ -42,8 +58,5 @@ int main()
         pthread_join(threads[i], NULL);
     }
 
-    printf("\n\nArray final multiplicado por 2: ");
-    for (int i = 0; i < array_size; i++) {
-        printf("%d ", array[i]);
-    }
+    testa_lab1(copia_array); // compara se array é o dobro de copia_array ( o array original ). 
 }
